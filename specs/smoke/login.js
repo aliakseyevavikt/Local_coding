41fd06476c76462ce5997f81d066c2b0b003aab0
akhunbabaeva <amcyuliya@gmail.com>
2020-09-21 23:44:18 -0700

    Login LayOut and login.js, negative, functional

--- specs/smoke/login.js
+++ specs/smoke/login.js
@@ -1,8 +1,8 @@
-import LoginPage from '../../../pageObjects/login.page';
-import ProfilePage from '../../../pageObjects/profile.page';
-import TopMenuPage from '../../../pageObjects/topMenu.page';
-import user from '../../../testData/user';
-import waitTime from '../../../testData/waitTimes';
+import LoginPage from '../../pageObjects/login.page';
+import ProfilePage from '../../pageObjects/profile.page';
+import TopMenuPage from '../../pageObjects/topMenu.page';
+import user from '../../testData/user';
+import waitTime from '../../testData/waitTimes';
 
 describe('LOGIN FUNCTIONALITY', function() {
 
@@ -10,25 +10,25 @@ describe('LOGIN FUNCTIONALITY', function() {
     LoginPage.open();
   });
 
-  it('TC-029 should verify that user can login as NEW USER', function() {
-    LoginPage.login(user.new.email, user.new.password);
+  it('Should verify that user can login as NEW USER', function() {
+    LoginPage.login(user.new2.email, user.new2.password);
     TopMenuPage.userAvatarName.waitForDisplayed({ timeout: waitTime.WAIT_TIME_MEDIUM });
     expect(ProfilePage.getLoginConfirmation()).eq(user.new.firstName + ' ' + user.new.lastName);
   });
 
-  it('TC-028 should verify that user can login as ADMIN', function() {
+  it('Should verify that user can login as ADMIN', function() {
     LoginPage.login(user.admin.email, user.admin.password);
     TopMenuPage.userAvatarName.waitForDisplayed({ timeout: waitTime.WAIT_TIME_MEDIUM });
     expect(ProfilePage.getLoginConfirmation()).eq(user.admin.firstName + ' ' + user.admin.lastName);
   });
 
-  it('TC-030 should verify that user can login as LEARNER', function() {
+  it('Should verify that user can login as LEARNER', function() {
     LoginPage.login(user.learner.email, user.learner.password);
     TopMenuPage.userAvatarName.waitForDisplayed({ timeout: waitTime.WAIT_TIME_MEDIUM });
     expect(ProfilePage.getLoginConfirmation()).eq(user.learner.firstName + ' ' + user.learner.lastName);
   });
 
-  it('TC-031 should verify that user can login as STUDENT', function() {
+  it('Should verify that user can login as STUDENT', function() {
     LoginPage.login(user.student.email, user.student.password);
     TopMenuPage.userAvatarName.waitForDisplayed({ timeout: waitTime.WAIT_TIME_MEDIUM });
     expect(ProfilePage.getLoginConfirmation()).eq(user.student.firstName + ' ' + user.student.lastName);
