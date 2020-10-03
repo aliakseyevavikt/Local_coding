48c3eb30c91def2efb839f33d865c180ddb11db3
Viktoryia A <alekseevavikt@gmail.com>
2020-10-03 11:31:29 -0700

    login updates

--- specs/smoke/login.js
+++ specs/smoke/login.js
@@ -4,17 +4,16 @@ import TopMenuPage from '../../pageObjects/topMenu.page';
 import user from '../../testData/userRoles';
 import waitTime from '../../testData/waitTimes';
 
-
-describe('LOGIN FUNCTIONALITY', function() {
   for (let property in user) {
-    before(() => {
-      LoginPage.open();
-      LoginPage.login(user[property].email, user[property].password);
-      TopMenuPage.userAvatarName.waitForDisplayed({ timeout: waitTime.WAIT_TIME_MEDIUM });
-    });
+    describe('LOGIN FUNCTIONALITY', function() {
+      before(() => {
+        LoginPage.open();
+        LoginPage.login(user[property].email, user[property].password);
+        TopMenuPage.userAvatarName.waitForDisplayed({ timeout: waitTime.WAIT_TIME_MEDIUM });
+      });
 
-    it(`${property} can login`, function() {
-      expect(ProfilePage.headerProfile.getText()).eq(user[property].firstName + ' ' + user[property].lastName);
+      it(`${property} can login`, function() {
+        expect(ProfilePage.headerProfile.getText()).eq(user[property].firstName + ' ' + user[property].lastName);
+      });
     });
   }
-});
